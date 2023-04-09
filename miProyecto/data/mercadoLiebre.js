ad7b462c496c7b1ad3b241d7fcea4c03dde3307e
Lucio Freixas <luciofreixas@MacBook-Pro-de-Lucio.local>
2023-04-09 20:33:44 -0300

    finalizacion de data mercadoliebre

--- miProyecto/data/mercadoLiebre.js
+++ miProyecto/data/mercadoLiebre.js
@@ -7,12 +7,61 @@ const mercadoLiebre = {
         numeroDocumento: 47248533,
         fotoPerfil: 'foto'
     },
-    productos: [{
+    productos: {
         imagen: 'a',
         nombreProducto: 'Notebook Asus',
         descripcion: 'La notebook Asus X515EA es una solución tanto para trabajar y estudiar como para entretenerte',
-        fechaCarga: 21/03/2023
+        fechaCarga: 21/03/2023,
 
+        imagen: 'a',
+        nombreProducto: 'Macbook Air M2',
+        descripcion: 'La notebook Macbook Air M2 es una solución tanto para trabajar y estudiar como para entretenerte',
+        fechaCarga: 21/03/2023,
+
+        imagen: 'a',
+        nombreProducto: 'Macbook Pro 13 inch',
+        descripcion: 'La Macbook Pro 13 inch es la mezcla de la innovacion y la eficazia',
+        fechaCarga: 21/03/2023,
+
+        imagen: 'a',
+        nombreProducto: 'Dell Inpiron',
+        descripcion: 'La Dell Inspiron es ideal para trabajar',
+        fechaCarga: 21/03/2023,
+
+        imagen: 'a',
+        nombreProducto: 'Acer Nitro 5',
+        descripcion: 'La Acer Nitro 5 es ideal para el gaming',
+        fechaCarga: 21/03/2023,
+
+        imagen: 'a',
+        nombreProducto: 'Aser Predator Elios 300',
+        descripcion: 'La Aser Predator Elios 300 es una laptop con refrigeracion avanzazda',
+        fechaCarga: 21/03/2023,
+
+        imagen: 'a',
+        nombreProducto: 'Dell Latitude',
+        descripcion: 'La Dell Latitude tiene la memoria ideal para el trabajo',
+        fechaCarga: 21/03/2023,
+
+        imagen: 'a',
+        nombreProducto: 'Asus Rog Strix',
+        descripcion: 'La Asus Rog Strix tiene el sistema de refrigeracion ideal para el gaming',
+        fechaCarga: 21/03/2023,
+
+        imagen: 'a',
+        nombreProducto: 'Macbook Air M1',
+        descripcion: 'Liviana y con un procesador veloz',
+        fechaCarga: 21/03/2023,
+
+        imagen: 'a',
+        nombreProducto: 'MacBook Pro 16 inch',
+        descripcion: 'La MacBook Pro 16 inch es perfecta para trabajar porque tiene un teclado amplio y rapido prosesador',
+        fechaCarga: 21/03/2023
+    },
+    comentarios: [{
+        nombreUsuario: 'Mateo Lopez Teran',
+        textoDelComentario: '',
+        imagen: 'a'
     }]
 }
 

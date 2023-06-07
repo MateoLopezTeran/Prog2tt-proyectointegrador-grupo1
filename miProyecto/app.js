4a1bedd72802918ac6f4dcf946da52feee0fe76e
Mateo Lopez Teran <mlopezteran@udesa.edu.ar>
2023-06-07 18:02:33 -0300

    app

--- miProyecto/app.js
+++ miProyecto/app.js
@@ -52,9 +52,7 @@ app.use(function(req, res, next) {
     }).catch((err) => {
       console.log(err);
     });
-  } else if(req.session.user != undefined){
-    console.log(req.session.user)
-    res.locals.user = req.session.user
+  } else {
     return next();
   }
   
